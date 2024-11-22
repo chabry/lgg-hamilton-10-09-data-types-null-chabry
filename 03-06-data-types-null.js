f748fcde215db0b933ad3f1d7eece656102b8a2d
Chabry <chabry01@gmail.com>
2024-11-22 14:51:44 +0100

    Exercice résolu

--- 03-06-data-types-null.js
+++ 03-06-data-types-null.js
@@ -3,16 +3,23 @@
 // Function 1
 function returnNull() {
   // Your code here
+  return null
 }
 
 // Function 2
 function checkIfNull(value) {
   // Your code here
+  if(value == null){
+    return true
+  }else{
+    return false
+  }
 }
 
 // Function 3
 function convertToNull(value) {
   // Your code here
+  return value = null
 }
 
 module.exports = {
